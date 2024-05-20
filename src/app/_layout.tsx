cf3043a46058f9c2bda27583b2453b5591542e6e
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-05-20 10:28:37 -0300

    chore: add raleway font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,24 @@
-import { View, Text, StatusBar } from "react-native";
+import { View, StatusBar } from "react-native";
 import React from "react";
 import { Slot } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
-  useFonts,
+  useFonts as useRoboto,
   Roboto_400Regular,
   Roboto_500Medium,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import {
+  useFonts as useRaleway,
+  Raleway_400Regular,
+  Raleway_700Bold,
+  Raleway_500Medium,
+  Raleway_600SemiBold,
+  Raleway_800ExtraBold,
+  Raleway_300Light,
+  Raleway_200ExtraLight,
+  Raleway_100Thin,
+} from "@expo-google-fonts/raleway";
 import * as SplashScreen from "expo-splash-screen";
 import "../styles/global.css";
 SplashScreen.preventAutoHideAsync();
@@ -15,13 +26,24 @@ SplashScreen.preventAutoHideAsync();
 const Layout = () => {
   const queryClient = new QueryClient();
 
-  const [fontsLoaded] = useFonts({
+  const [RobotoFontsLoaded] = useRoboto({
     Roboto_400Regular,
     Roboto_500Medium,
     Roboto_700Bold,
   });
 
-  if (fontsLoaded) {
+  const [RalewayFontsLoaded] = useRaleway({
+    Raleway_400Regular,
+    Raleway_700Bold,
+    Raleway_500Medium,
+    Raleway_600SemiBold,
+    Raleway_800ExtraBold,
+    Raleway_300Light,
+    Raleway_200ExtraLight,
+    Raleway_100Thin,
+  });
+
+  if (RobotoFontsLoaded && RalewayFontsLoaded) {
     SplashScreen.hideAsync();
   }
 
@@ -29,7 +51,7 @@ const Layout = () => {
     <QueryClientProvider client={queryClient}>
       <View style={{ flex: 1 }}>
         <StatusBar barStyle="light-content" />
-        {fontsLoaded && <Slot />}
+        {RobotoFontsLoaded && RalewayFontsLoaded && <Slot />}
       </View>
     </QueryClientProvider>
   );
--- src/theme/fontFamily.ts
+++ src/theme/fontFamily.ts
@@ -2,4 +2,13 @@ export const fontFamily = {
   regular: "Roboto_400Regular",
   medium: "Roboto_500Medium",
   bold: "Roboto_700Bold",
+
+  ralewayRegular: "Raleway_400Regular",
+  ralewayMedium: "Raleway_500Medium",
+  ralewaySemiBold: "Raleway_600SemiBold",
+  ralewayExtraBold: "Raleway_800ExtraBold",
+  ralewayLight: "Raleway_300Light",
+  ralewayExtraLight: "Raleway_200ExtraLight",
+  ralewayThin: "Raleway_100Thin",
+  ralewayBold: "Raleway_700Bold",
 };
