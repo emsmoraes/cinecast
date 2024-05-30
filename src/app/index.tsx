5fc64f505b4662d06929dcc409349f5ea6ab917c
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-05-30 09:17:20 -0300

    feat: add login page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,67 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import React from "react";
+import { Input } from "@/components/Input";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { router } from "expo-router";
 
 const Index = () => {
+  const [loading, setLoading] = React.useState(false);
+
+  const navigate = router;
+
+  const login = () => {
+    setLoading(true);
+    setTimeout(() => {
+      navigate.push("(tabs)");
+    }, 2000);
+  };
+
   return (
-    <View>
-      <Text>index</Text>
+    <View className="flex-1 bg-primaryZinc-900 px-5 pt-16">
+      <Image
+        source={require("../assets/images/logo.png")}
+        className="h-20 w-20"
+      />
+      <View className="pt-20">
+        <Text className="text-center font-ralewaySemiBold text-[19px] text-white">
+          Seus filmes e series favoritos {"\n"}{" "}
+          <Text className="text-[24px] text-primaryRed-900/80 ">
+            em um só lugar
+          </Text>
+        </Text>
+
+        <View className="mt-16 gap-4">
+          <Input
+            placeholder="Insira seu email"
+            className="rounded-2xl border border-zinc-600/10 bg-zinc-800/20 px-4 py-3 text-[16px] text-white"
+          />
+          <Input
+            placeholder="Insira sua senha"
+            className="rounded-2xl border border-zinc-600/10 bg-zinc-800/20 px-4 py-3 text-[16px] text-white"
+            secureTextEntry={true}
+          />
+          <Pressable
+            disabled={loading}
+            onPress={login}
+            className="mt-3 w-full flex-row items-center justify-center gap-2 rounded-2xl bg-primaryRed-900/80 px-4 py-3"
+          >
+            <Text className="text-center font-ralewaySemiBold text-[16px] text-white">
+              {loading ? "Entrando..." : "Entrar"}
+            </Text>
+            {loading ? (
+              <AntDesign
+                name="loading1"
+                className="animate-spin"
+                size={19}
+                color="white"
+              />
+            ) : (
+              <MaterialIcons name="arrow-forward-ios" size={19} color="white" />
+            )}
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 };
---
+++ src/components/Input.tsx
@@ -0,0 +1,25 @@
+import { forwardRef } from "react";
+import { Text, TextInput, View } from "react-native";
+import React from "react";
+import { cn } from "../lib/utils";
+
+export interface InputProps
+  extends React.ComponentPropsWithoutRef<typeof TextInput> {
+  label?: string;
+  labelClasses?: string;
+}
+const Input = forwardRef<React.ElementRef<typeof TextInput>, InputProps>(
+  ({ className, label, labelClasses, ...props }) => (
+    <View className={cn("flex flex-col gap-1.5")}>
+      {label && <Text className={cn("text-base", labelClasses)}>{label}</Text>}
+      <TextInput
+        className={cn(className, "rounded-lg border px-4 py-2.5")}
+        {...props}
+      />
+    </View>
+  ),
+);
+
+Input.displayName = "Input";
+
+export { Input };
